3d89efd495013e83df2051519ad903f92e23b9f8
Ricardo Dirani <Ricardo.Dirani1@kyndryl.com>
2023-09-08 14:14:39 -0300

    Update Movie.java

--- src/main/java/challenge/domain/Movie.java
+++ src/main/java/challenge/domain/Movie.java
@@ -1,26 +1,14 @@
 package challenge.domain;
 
-import jakarta.persistence.*;
-//        Create an endpoint to consume an API from TBMD to retrieve all the movies [(https://imdb-api.com/) ](https://developer.themoviedb.org/)and populate your database (you can use WebClient or Feign to consume the API). This API could be called just by the ADMIN user, see @Secured and Roles in Spring Security.
-//        Create an endpoint to list all the movies.
-//        Create an endpoint to include a movie to the user (favorite list)
-//        Create an endpoint to exclude the movie from the favorite list
-//        Each time the user includes the movie in the favorite list add one "star" to the movie
-//        Create an endpoint to list the top 10 movies, the movies with more stars.
-//        Create an endpoint to list the favorite movies per user.
-//        Don't forget to include Swagger/OpenAPI, and the test.
-/*
-    user -> favorite list (movie) set
-
-
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "movies")
 public class Movie {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private int releaseYear;
@@ -28,14 +16,6 @@ public class Movie {
     private double rating;
 
     public Movie() {
-    }
-
-    public Movie(Long id, String title, int releaseYear, String genre, double rating) {
-        this.id = id;
-        this.title = title;
-        this.releaseYear = releaseYear;
-        this.genre = genre;
-        this.rating = rating;
     }
 
     // Getter and setter methods for id, title, releaseYear, genre, and rating
@@ -52,4 +32,3 @@ public class Movie {
                 '}';
     }
 }
-
