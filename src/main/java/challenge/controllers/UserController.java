0bf2c6ea48a13b9a32c47f3feb3e91efb3f88211
Ricardo Dirani <dirani@gmail.com>
2023-09-08 09:43:22 -0300

    Update UserController.java

--- src/main/java/challenge/controllers/UserController.java
+++ src/main/java/challenge/controllers/UserController.java
@@ -14,24 +14,25 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController
 {
-    //autowired the StudentService class
+    //autowired the UserService class
     @Autowired
     UserService userService;
-    //creating a get mapping that retrieves all the students detail from the database
+    //autowired the RoleService class
     @Autowired
     RoleService roleService;
+    //creating a get mapping that retrieves all the user detail from the database
     @GetMapping
-    private List<User> getAllStudent()
+    private List<User> getAllUsers()
     {
         return userService.getAllUser();
     }
-    //creating a get mapping that retrieves the detail of a specific student
+    //creating a get mapping that retrieves the detail of a specific user
     @GetMapping("/{id}")
     private User getUser(@PathVariable("id") Long id)
     {
         return userService.getUserById(id);
     }
-    //creating a delete mapping that deletes a specific student
+    //creating a delete mapping that deletes a specific user
     @DeleteMapping("/{id}")
     private void deleteUser(@PathVariable("id") Long id)
     {
